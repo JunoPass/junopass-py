7f928547b084585f81710e6ef2ba9ede5c3c1f0a
Felix Cheruiyot <felix.cheruiyot@kenyaapps.net>
2020-05-07 17:45:32 +0300

    Update setup requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="junopass-py",
-    version="1.0.0",
+    version="1.0.2",
     author="Felix Cheruiyot",
     author_email="felix@junopass.com",
     description="JunoPass support for Python.JunoPass provides a secure 2FA and Passwordless authentication.",
@@ -15,10 +15,14 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Development Status :: 4 - Beta", 
-        "Intended Audience :: Developers", 
-        "Topic :: Security", 
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Security",
         "License :: OSI Approved :: MIT License",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        'PyNaCl==1.3.0',
+        'requests==2.23.0'
+    ]
 )
